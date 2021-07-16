59a38156a169492311d15ab4ee0cec42029ef151
Brian Tang <ashrielbrian@gmail.com>
2021-07-16 23:09:51 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# Predict Customer Churn
+
+- Project **Predict Customer Churn** of ML DevOps Engineer Nanodegree Udacity
+
+## Project Description
+Your project description here.
+
+
+## Running Files
+How do you run your files? What should happen when you run your files?
+
+
+# Getting Started
+
+```bash
+
+    python3 -m venv .venv
+    source .venv/bin/activate
+    pip install -r requirements.txt
+
+```
+
+Additionally, if you'd like to run the `churn_notebook.ipynb`, then you'll need to run `pip install ipykernel`.
---
+++ churn_library.py
@@ -0,0 +1,356 @@
+"""
+    Module to predict customer churn using Logistic Regresstion
+    and Random Forest classifier models.
+
+    Author: Brian Tang
+    Date: July 2021
+"""
+import shap
+import joblib
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns; sns.set()
+
+from sklearn.preprocessing import normalize
+from sklearn.model_selection import train_test_split
+
+from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import GridSearchCV
+
+from sklearn.metrics import plot_roc_curve, classification_report
+import logging
+
+from constants import IMAGES_DIR, CAT_COLUMNS, KEEP_COLUMNS
+
+logging.basicConfig(filename='logs/churn_library.log', 
+    filemode='w', 
+    format='%(asctime)s - %(levelname)s: %(message)s',
+    level=logging.INFO)
+
+def import_data(pth, **kwargs):
+    '''
+        Returns dataframe for the csv found at pth
+        input:
+            pth: a path to the csv
+        output:
+            df: pandas dataframe
+    '''
+    try:
+        logging.info(f'Loading data from {pth}...')
+        df = pd.read_csv(pth, **kwargs)
+        return df
+    except FileNotFoundError as e:
+        logging.exception('churn_library')
+        logging.error(f'No file found at {pth}. Exiting...')
+        raise e
+
+def generate_histograms(df, category_lst, xlabels, target_dir):
+    '''
+        Saves histograms of dataframe categories
+        input:
+            df: pandas dataframe
+            category_lst: list of columns that contain categorical features
+            xlabels: list of strings to set as the histogram xlabel
+            target_dir: directory to save histograms to
+        output:
+            None
+    '''
+    for cat, xlabel in zip(category_lst, xlabels):
+        filename = f'{cat.lower()}_histogram.jpg'
+        try:
+            logging.info(f'Attempting to generate histogram for {cat}.')
+            plt.figure(figsize=(20,10))
+            df[cat].hist()
+            plt.ylabel('Number of Customers')
+            plt.xlabel(xlabel)
+            plt.savefig(f'{target_dir}/{filename}')
+            logging.info(f'Saved histogram: {filename}.')
+        except KeyError:
+            logging.error(f'{cat} category not found in DataFrame.')
+    return None
+
+def perform_eda(df, histogram_category_lst, target_dir):
+    '''
+        Perform eda on df and save figures to images folder
+        input:
+            df: pandas dataframe
+            histogram_category_lst: list of columns that contain categorical features
+            target_dir: directory to save results to
+        output:
+            None
+    '''
+    logging.info('Performing exploratory data analysis...')
+    generate_histograms(df, histogram_category_lst, histogram_category_lst, target_dir)
+
+    ms_filename = 'marital_status_bar_chart.jpg'
+    plt.figure(figsize=(20,10)) 
+    df.Marital_Status.value_counts('normalize').plot(kind='bar')
+    plt.savefig(f'{target_dir}/{ms_filename}')
+    logging.info(f'Saved {ms_filename}')
+
+    tt_filename = 'total_trans_ct.jpg'
+    plt.figure(figsize=(20,10)) 
+    sns.distplot(df['Total_Trans_Ct'])
+    plt.savefig(f'{target_dir}/{tt_filename}')
+    logging.info(f'Saved {tt_filename}')
+
+    hm_filename = 'heat_map.jpg'
+    plt.figure(figsize=(20,10)) 
+    sns.heatmap(df.corr(), annot=False, cmap='Dark2_r', linewidths = 2)
+    plt.savefig(f'{target_dir}/{hm_filename}', bbox_inches='tight')
+    logging.info(f'Saved {hm_filename}')
+
+def encoder_helper(df, category_lst, response='Churn'):
+    '''
+        Helper function to turn each categorical column into a new column with
+        proportion of churn for each category - associated with cell 15 from the notebook
+        input:
+                df: pandas dataframe
+                category_lst: list of columns that contain categorical features
+                response: string of response name [optional argument that could be used for naming variables or index y column]
+        output:
+                df: pandas dataframe with new columns for
+    '''
+
+    df[response] = df['Attrition_Flag'].apply(lambda val: 0 if val == "Existing Customer" else 1)
+    for cat in category_lst:
+        try:
+            logging.info(f'Encoding categorical variable: {cat}')
+            cat_groups = df.groupby(cat).mean()[response]
+            df[cat + f'_{response}'] = df[cat].apply(lambda val: cat_groups.loc[val])
+        except KeyError:
+            logging.error(f'{cat} category not found in DataFrame.')
+    return df
+
+
+def perform_feature_engineering(df, target_feature, test_size=0.3):
+    '''
+        Runs feature engineering to keep columns and returns train/test data
+        input:
+            df: pandas dataframe
+            response: string of response name [optional argument that could be used for naming variables or index y column]
+
+        output:
+            X_train: X training data
+            X_test: X testing data
+            y_train: y training data
+            y_test: y testing data
+    '''
+    logging.info(f'Performing feature engineering. Keeping columns: {", ".join(KEEP_COLUMNS)}.')
+    logging.info(f'Target feature is: `{target_feature}`')
+    X = pd.DataFrame()
+    y = df[target_feature]
+    X[KEEP_COLUMNS] = df[KEEP_COLUMNS]
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size, random_state=42)
+    return X_train, X_test, y_train, y_test
+
+def classification_report_image(y_train,
+                                y_test,
+                                y_train_preds_lr,
+                                y_train_preds_rf,
+                                y_test_preds_lr,
+                                y_test_preds_rf,
+                                target_dir):
+    '''
+        Produces classification report for training and testing results and stores report as image
+        in images folder
+        input:
+            y_train: training response values
+            y_test:  test response values
+            y_train_preds_lr: training predictions from logistic regression
+            y_train_preds_rf: training predictions from random forest
+            y_test_preds_lr: test predictions from logistic regression
+            y_test_preds_rf: test predictions from random forest
+            target_dir: directory to save image results to
+        output:
+            None
+    '''
+    # plots Random Forest model classification report
+    rfc_filename = 'random_forest_cr.jpg'
+    plt.figure(figsize=(5, 5))
+    plt.text(0.01, 1.25, str('Random Forest Train'), {'fontsize': 10}, fontproperties = 'monospace')
+    plt.text(0.01, 0.7, str(classification_report(y_train, y_train_preds_rf)), {'fontsize': 10}, fontproperties = 'monospace')
+    plt.text(0.01, 0.6, str('Random Forest Test'), {'fontsize': 10}, fontproperties = 'monospace')
+    plt.text(0.01, 0.05, str(classification_report(y_test, y_test_preds_rf)), {'fontsize': 10}, fontproperties = 'monospace')
+    plt.axis('off')
+    plt.savefig(f'{target_dir}/{rfc_filename}', bbox_inches='tight')
+    logging.info(f'Saved Random Forest classification report: {rfc_filename}')
+
+    # plots Logistic Regression model classification Report
+    lr_filename = 'logistic_regression_cr.jpg'
+    plt.figure(figsize=(5, 5))
+    plt.text(0.01, 1.25, str('Logistic Regression Train'), {'fontsize': 10}, fontproperties = 'monospace')
+    plt.text(0.01, 0.05, str(classification_report(y_train, y_train_preds_lr)), {'fontsize': 10}, fontproperties = 'monospace')
+    plt.text(0.01, 0.6, str('Logistic Regression Test'), {'fontsize': 10}, fontproperties = 'monospace')
+    plt.text(0.01, 0.7, str(classification_report(y_test, y_test_preds_lr)), {'fontsize': 10}, fontproperties = 'monospace')
+    plt.axis('off')
+    plt.savefig(f'{target_dir}/{lr_filename}', bbox_inches='tight')
+    logging.info(f'Saved Logistic Regression classification report: {lr_filename}')
+
+def roc_plot(lr_model, rfc_model, X_test, y_test, target_dir):
+    """
+        Saves the ROC results for the LR and RFC models on the same plot
+        input:
+            lr_model: training response values
+            rfc_model:  test response values
+            X_test: training predictions from logistic regression
+            y_test: training predictions from random forest
+            target_dir: directory to save image results t
+        output:
+            None
+    """
+    roc_filename = 'roc_plot.jpg'
+    lrc_plot = plot_roc_curve(lr_model, X_test, y_test)
+    plt.figure(figsize=(15,8))
+    ax = plt.gca()
+    _ = plot_roc_curve(rfc_model, X_test, y_test, ax=ax, alpha=0.8)
+    lrc_plot.plot(ax=ax, alpha=0.8)
+    plt.savefig(f'{target_dir}/{roc_filename}')
+    logging.info(f'Saved ROC plot: {roc_filename}')
+
+def feature_importance_plot(model, X_data, target_dir):
+    '''
+        Creates and stores the feature importances in pth
+        input:
+            model: model object containing feature_importances_
+            X_data: pandas dataframe of X values
+            output_pth: path to store the figure
+        output:
+            None
+    '''
+    # Calculate feature importances
+    importances = model.feature_importances_
+    # Sort feature importances in descending order
+    indices = np.argsort(importances)[::-1]
+
+    # Rearrange feature names so they match the sorted feature importances
+    names = [X_data.columns[i] for i in indices]
+
+    # Create plot
+    plt.figure(figsize=(20,5))
+
+    # Create plot title
+    plt.title("Feature Importance")
+    plt.ylabel('Importance')
+
+    # Add bars
+    plt.bar(range(X_data.shape[1]), importances[indices])
+
+    # Add feature names as x-axis labels
+    plt.xticks(range(X_data.shape[1]), names, rotation=90)
+
+    filename = 'random_forest_feature_importance.jpg'
+    plt.savefig(f'{target_dir}/{filename}', bbox_inches='tight')
+    logging.info(f'Saved feature importance plot: {filename}')
+
+def train_random_forest_classifier(X_train, y_train, param_grid=None):
+    """ 
+        Initializes and trains a random forest classifier
+        input:
+            X_train: X training data
+            y_train: y training data
+            param_grid: [optional] optional params for the RFC model
+        output:
+            trained model: sklearn.linear_model.LogisticRegression
+    """
+    logging.info('Initialising Random Forest classifier...')
+    rfc = RandomForestClassifier(random_state=42)
+    if param_grid is None:
+        param_grid = { 
+            'n_estimators': [200, 500],
+            'max_features': ['auto', 'sqrt'],
+            'max_depth' : [4,5,100],
+            'criterion' :['gini', 'entropy']
+        }
+
+    cv_rfc = GridSearchCV(estimator=rfc, param_grid=param_grid, cv=5)
+    logging.info('Training RFC. Please wait...')
+    cv_rfc.fit(X_train, y_train)
+    return cv_rfc
+
+def train_logistic_regression(X_train, y_train, solver='liblinear'):
+    """ 
+        initializes and trains a logistic regression model
+        input:
+            X_train: X training data
+            y_train: y training data
+            solver: [optional] the solver to be used in the LR model
+        output:
+            trained model: sklearn.linear_model.LogisticRegression
+    """
+    logging.info(f'Initialising Logistic Regression with {solver}.')
+    lrc = LogisticRegression(solver=solver)
+    logging.info('Training LR. Please wait...')
+    lrc.fit(X_train, y_train)
+    return lrc
+
+def train_models(X_train, X_test, y_train, y_test):
+    '''
+        Train, store model results: images + scores, and store models
+        input:
+            X_train: X training data
+            X_test: X testing data
+            y_train: y training data
+            y_test: y testing data
+        output:
+            None
+    '''
+    cv_rfc = train_random_forest_classifier(X_train, y_train)
+    lrc = train_logistic_regression(X_train, y_train)
+
+    # store models
+    try:
+        joblib.dump(cv_rfc.best_estimator_, './models/rfc_model.pkl')
+        joblib.dump(lrc, './models/logistic_model.pkl')
+        logging.info('Models saved successfully.')
+    except Exception as e:
+        logging.error('Error saving models. Exiting.')
+        logging.exception('churn_library')
+        raise e
+    return
+
+def load_model(model_pth):
+    '''
+        Loads the model given its path
+        input:
+            model_pth: string path to the model's location in the filesystem
+        output:
+            model: an sklearn model
+    '''
+    return joblib.load(model_pth)
+
+if __name__ == '__main__':
+    # define directory paths
+    EDA_DIR = IMAGES_DIR + '/eda'
+    RESULTS_DIR  = IMAGES_DIR + '/results'
+
+    # preprocess data
+    df = import_data('data/bank_data.csv')
+    df = encoder_helper(df, CAT_COLUMNS)
+    perform_eda(df, ['Churn', 'Customer_Age'], EDA_DIR)
+
+    X_train, X_test, y_train, y_test = perform_feature_engineering(df, target_feature='Churn')
+    
+    # model training
+    train_models(X_train, X_test, y_train, y_test)
+    
+    # reload models for prediction and reporting
+    rfc_model = load_model('./models/rfc_model.pkl')
+    lr_model = load_model('./models/logistic_model.pkl')
+    
+    # run predictions
+    y_train_preds_lr = lr_model.predict(X_train)
+    y_test_preds_lr = lr_model.predict(X_test)
+
+    y_train_preds_rf = rfc_model.predict(X_train)
+    y_test_preds_rf = rfc_model.predict(X_test)
+
+    # reporting and plotting results
+    classification_report_image(y_train, y_test, y_train_preds_lr,
+                                y_train_preds_rf, y_test_preds_lr,
+                                y_test_preds_rf, target_dir=RESULTS_DIR)
+    
+    feature_importance_plot(rfc_model, X_train, target_dir=RESULTS_DIR)
+    roc_plot(lr_model, rfc_model, X_test, y_test, target_dir=RESULTS_DIR)
